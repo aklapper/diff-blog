bda71128fefd7a8d304cc2bbed84aef6d6715a29
K. Adam White <kadamwhite@users.noreply.github.com>
2022-04-28 10:32:14 -0400

    Upgrade the-events-calendar to 5.14.2 (from 5.8.1)

--- plugins/the-events-calendar/common/src/Tribe/Admin/Notice/Service_Provider.php
+++ plugins/the-events-calendar/common/src/Tribe/Admin/Notice/Service_Provider.php
@@ -2,7 +2,7 @@
 /**
  * Handles admin notice functions.
  *
- * @since   TBD
+ * @since   4.14.2
  *
  * @package Tribe\Admin\Notice;
  */
@@ -12,7 +12,7 @@ namespace Tribe\Admin\Notice;
 /**
  * Class Notice
  *
- * @since TBD
+ * @since 4.14.2
  *
  * @package Tribe\Admin\Notice
  */
@@ -21,13 +21,12 @@ class Service_Provider extends \tad_DI52_ServiceProvider {
 	/**
 	 * Registers the objects and filters required by the provider to manage admin notices.
 	 *
-	 * @since TBD
+	 * @since 4.14.2
 	 */
 	public function register() {
 		tribe_singleton( 'pue.notices', 'Tribe__PUE__Notices' );
 		tribe_singleton( WP_Version::class, WP_Version::class, [ 'hook' ] );
 		tribe_singleton( 'admin.notice.php.version', 'Tribe__Admin__Notice__Php_Version', [ 'hook' ] );
-		tribe_singleton( 'admin.notice.marketing', 'Tribe__Admin__Notice__Marketing', [ 'hook' ] );
 		tribe_singleton( Marketing\Stellar_Sale::class, Marketing\Stellar_Sale::class, [ 'hook' ] );
 
 		$this->hooks();
@@ -36,7 +35,7 @@ class Service_Provider extends \tad_DI52_ServiceProvider {
 	/**
 	 * Set up hooks for classes.
 	 *
-	 * @since TBD
+	 * @since 4.14.2
 	 */
 	private function hooks() {
 		add_action( 'tribe_plugins_loaded', [ $this, 'plugins_loaded'] );
@@ -45,7 +44,7 @@ class Service_Provider extends \tad_DI52_ServiceProvider {
 	/**
 	 * Setup for things that require plugins loaded first.
 	 *
-	 * @since TBD
+	 * @since 4.14.2
 	 */
 	public function plugins_loaded() {
 		tribe( 'pue.notices' );
@@ -58,5 +57,6 @@ class Service_Provider extends \tad_DI52_ServiceProvider {
 
 		tribe( Marketing\Stellar_Sale::class );
 		tribe( Marketing\Black_Friday::class );
+		tribe( Marketing\End_Of_Year_Sale::class );
 	}
 }
