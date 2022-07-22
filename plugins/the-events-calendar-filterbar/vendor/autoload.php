c3b42c26cc5933e1238e2fcf00d1f037c05dbb49
K. Adam White <kadamwhite@users.noreply.github.com>
2022-07-22 13:05:01 -0400

    Update paid plugin the-events-calendar-filterbar from 5.2.1 -> 5.3.1

--- plugins/the-events-calendar-filterbar/vendor/autoload.php
+++ plugins/the-events-calendar-filterbar/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit61df6b1957d06b1cbd2234607839d7ba::getLoader();
+return ComposerAutoloaderInite41b3cdc3b4c011c75cf39165ed4622e::getLoader();
