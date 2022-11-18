a1b2fe4e8c092fe7c8ef97ae8e786a99a56d1143
Alexis J. Villegas <14046668+ajvillegas@users.noreply.github.com>
2022-11-18 15:32:51 -0400

    Update plugin to prevent admins from having to agree

--- client-mu-plugins/diff-blocks/src/plugins/content-licensing-notice/index.js
+++ client-mu-plugins/diff-blocks/src/plugins/content-licensing-notice/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { CheckboxControl } from '@wordpress/components';
-import { useDispatch } from '@wordpress/data';
+import { useDispatch, useSelect } from '@wordpress/data';
 import { PluginPrePublishPanel } from '@wordpress/edit-post';
 import { useState, useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
@@ -14,10 +14,23 @@ import { __ } from '@wordpress/i18n';
 const ContentLicensingNotice = () => {
 	const [ agreed, setDisclaimerAgreement ] = useState( false );
 	const { lockPostSaving, unlockPostSaving } = useDispatch( 'core/editor' );
+	const { postStatus, currentUserRoles } = useSelect( ( select ) => {
+		const currentUser = select( 'core' ).getCurrentUser();
+		const postStatus = select( 'core/editor' ).getEditedPostAttribute( 'status' );
+		const currentUserRoles = currentUser && currentUser.id
+			? select( 'core' ).getUser( currentUser.id )?.roles || []
+			: [];
+
+		return {
+			postStatus,
+			currentUserRoles,
+		};
+	} );
+	const shouldLock = ! agreed && ( postStatus === 'draft' || ! currentUserRoles.includes( 'administrator' ) );
 
 	// Put all the logic in the useEffect hook.
 	useEffect( () => {
-		if ( ! agreed ) {
+		if ( shouldLock ) {
 			lockPostSaving();
 		} else {
 			unlockPostSaving();
@@ -25,10 +38,13 @@ const ContentLicensingNotice = () => {
 
 		// The dispatchers are static references, safe to ignore.
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [ agreed ] );
+	}, [ shouldLock ] );
 
-	return (
-		<PluginPrePublishPanel title={ __( 'Publish Checklist', 'diff-blocks' ) } initialOpen>
+	const showCheckbox = postStatus === 'draft' && ! currentUserRoles.includes( 'administrator' );
+
+	let notice;
+	if ( showCheckbox ) {
+		notice = (
 			<CheckboxControl
 				checked={ agreed }
 				onChange={ setDisclaimerAgreement }
@@ -41,6 +57,23 @@ const ContentLicensingNotice = () => {
 				}
 				label={ __( 'By clicking publish, you agree to license your work under the CC BY-SA 3.0 license. Please be aware that this license is irrevocable and allows others to use and remix your work off of Wikimedia websites.', 'diff-blocks' ) }
 			/>
+		);
+	} else {
+		notice = (
+			<>
+				<p>{ __( 'You agree to license your work under the CC BY-SA 3.0 license. Please be aware that this license is irrevocable and allows others to use and remix your work off of Wikimedia websites.', 'diff-blocks' ) }</p>
+				<p>
+					<a target="_blank" rel="noopener noreferrer" href="https://creativecommons.org/licenses/by-sa/3.0/">
+						{ __( 'CC BY-SA 3.0 license', 'diff-blocks' ) }
+					</a>
+				</p>
+			</>
+		);
+	}
+
+	return (
+		<PluginPrePublishPanel title={ __( 'Publish Checklist', 'diff-blocks' ) } initialOpen>
+			{ notice }
 		</PluginPrePublishPanel>
 	);
 };
