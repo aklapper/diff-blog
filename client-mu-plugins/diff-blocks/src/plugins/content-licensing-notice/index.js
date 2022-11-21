d807a6b1101e4026871213eb84012b3de4cb2d43
Alexis J. Villegas <14046668+ajvillegas@users.noreply.github.com>
2022-11-21 13:09:15 -0400

    Update conditional and code syntax

--- client-mu-plugins/diff-blocks/src/plugins/content-licensing-notice/index.js
+++ client-mu-plugins/diff-blocks/src/plugins/content-licensing-notice/index.js
@@ -26,7 +26,8 @@ const ContentLicensingNotice = () => {
 			currentUserRoles,
 		};
 	} );
-	const shouldLock = ! agreed && ( postStatus === 'draft' || ! currentUserRoles.includes( 'administrator' ) );
+	const showCheckbox = postStatus === 'draft' && ! currentUserRoles.includes( 'administrator' );
+	const shouldLock = showCheckbox && ! agreed;
 
 	// Put all the logic in the useEffect hook.
 	useEffect( () => {
@@ -40,40 +41,31 @@ const ContentLicensingNotice = () => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [ shouldLock ] );
 
-	const showCheckbox = postStatus === 'draft' && ! currentUserRoles.includes( 'administrator' );
-
-	let notice;
-	if ( showCheckbox ) {
-		notice = (
-			<CheckboxControl
-				checked={ agreed }
-				onChange={ setDisclaimerAgreement }
-				help={
+	return (
+		<PluginPrePublishPanel title={ __( 'Publish Checklist', 'diff-blocks' ) } initialOpen>
+			{  showCheckbox ? (
+				<CheckboxControl
+					checked={ agreed }
+					onChange={ setDisclaimerAgreement }
+					help={
+						<p>
+							<a target="_blank" rel="noopener noreferrer" href="https://creativecommons.org/licenses/by-sa/3.0/">
+								{ __( 'CC BY-SA 3.0 license', 'diff-blocks' ) }
+							</a>
+						</p>
+					}
+					label={ __( 'By clicking publish, you agree to license your work under the CC BY-SA 3.0 license. Please be aware that this license is irrevocable and allows others to use and remix your work off of Wikimedia websites.', 'diff-blocks' ) }
+				/>
+			) : (
+				<>
+					<p>{ __( 'You agree to license your work under the CC BY-SA 3.0 license. Please be aware that this license is irrevocable and allows others to use and remix your work off of Wikimedia websites.', 'diff-blocks' ) }</p>
 					<p>
 						<a target="_blank" rel="noopener noreferrer" href="https://creativecommons.org/licenses/by-sa/3.0/">
 							{ __( 'CC BY-SA 3.0 license', 'diff-blocks' ) }
 						</a>
 					</p>
-				}
-				label={ __( 'By clicking publish, you agree to license your work under the CC BY-SA 3.0 license. Please be aware that this license is irrevocable and allows others to use and remix your work off of Wikimedia websites.', 'diff-blocks' ) }
-			/>
-		);
-	} else {
-		notice = (
-			<>
-				<p>{ __( 'You agree to license your work under the CC BY-SA 3.0 license. Please be aware that this license is irrevocable and allows others to use and remix your work off of Wikimedia websites.', 'diff-blocks' ) }</p>
-				<p>
-					<a target="_blank" rel="noopener noreferrer" href="https://creativecommons.org/licenses/by-sa/3.0/">
-						{ __( 'CC BY-SA 3.0 license', 'diff-blocks' ) }
-					</a>
-				</p>
-			</>
-		);
-	}
-
-	return (
-		<PluginPrePublishPanel title={ __( 'Publish Checklist', 'diff-blocks' ) } initialOpen>
-			{ notice }
+				</>
+			) }
 		</PluginPrePublishPanel>
 	);
 };
