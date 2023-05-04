0aee0e5c0a12a040bc1d3c633227ebf48dd607b8
Alexis J. Villegas <14046668+ajvillegas@users.noreply.github.com>
2023-05-04 17:57:33 -0400

    Remove loop in favor of a query string

--- client-mu-plugins/diff-comments/inc/class-remove-comment-author-ip.php
+++ client-mu-plugins/diff-comments/inc/class-remove-comment-author-ip.php
@@ -17,67 +17,45 @@ use WP_CLI_Command;
  */
 class Remove_Comment_Author_IP extends WP_CLI_Command {
 	/**
-	 * Remove recorded comment author IP address from the database.
+	 * Remove comment author IP addresses from the database.
 	 *
 	 * ## OPTIONS
 	 *
-	 * [--dry-run=<dry-run>]
-	 * : Perform a test run if true, make changes to the database if false.
-	 * ---
-	 * default: true
-	 * options:
-	 *   - true
-	 *   - false
-	 * ---
+	 * [--dry-run]
+	 * : Run the command without committing changes.
+	 *
+	 * [--ip-string]
+	 * : An optional string to replace the IP address in all comments.
 	 *
 	 * ## EXAMPLES
 	 *
-	 *    wp diff-comments-remove-ip --dry-run=false
+	 *    wp comments remove-ip --dry-run
+	 *    wp comments remove-ip --ip-string="IP Removed for Privacy"
 	 *
 	 * @param array $args List of arguments passed to the commands.
 	 * @param array $assoc_args Arguments passed to the command parsed into key/value pairs.
 	 */
 	public function __invoke( $args, $assoc_args ) {
-		$count = 0;
+		global $wpdb;
 
-		// If --dry-run is not set, then it will default to true.
-		// Must set --dry-run explicitly to false to run this command.
-		$dry_run = filter_var( $assoc_args['dry-run'] ?? true, FILTER_VALIDATE_BOOLEAN );
-
-		if ( ! $dry_run ) {
-			WP_CLI::warning( 'Dry run is disabled, data in the database will be modified.' );
-		}
+		$dry_run   = \WP_CLI\Utils\get_flag_value( $assoc_args, 'dry-run' );
+		$ip_string = \WP_CLI\Utils\get_flag_value( $assoc_args, 'ip-string' );
 
 		// Get all comments with recorded IP.
 		$comments = $this->get_comments_with_ip();
 
 		if ( count( $comments ) > 1 ) {
-			$label    = $dry_run ? 'Querying Comments' : 'Deleting ' . count( $comments ) . ' Recorded IP Addresses';
-			$progress = \WP_CLI\Utils\make_progress_bar( $label, count( $comments ) );
-
-			foreach ( $comments as $comment ) {
-				$update_comment = [
-					'comment_ID'        => $comment,
-					'comment_author_IP' => '',
-				];
-
-				if ( ! $dry_run ) {
-					wp_update_comment( $update_comment );
-				}
-
-				$count++;
-				$progress->tick();
-			}
-
-			$progress->finish();
-
-			if ( $dry_run === true ) {
-				WP_CLI::success( sprintf( '%d comments would have had the author IP removed if this was not a dry run.', $count ) );
+			if ( $dry_run ) {
+				WP_CLI::success( sprintf( '%d comments will have the author IP removed.', count( $comments ) ) );
 			} else {
-				WP_CLI::success( sprintf( '%d comments had the author IP removed.', $count ) );
+				WP_CLI::confirm( 'Are you sure you want to delete the IP address from ' . count( $comments ) . ' comments in the database?', $assoc_args );
+
+				$wpdb->query( "UPDATE wp_comments SET comment_author_IP = '$ip_string'" );
+
+				WP_CLI::success( sprintf( '%d comments had the author IP removed.', count( $comments ) ) );
 			}
 		} else {
-			WP_CLI::line( 'No comments found.' );
+			WP_CLI::line( 'No comments with recorded IP found.' );
 		}
 	}
 
