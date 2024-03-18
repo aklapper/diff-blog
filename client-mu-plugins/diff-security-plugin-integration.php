5a7e153c7e5a6f3cf21eb3c8031cc7e87dc5c1a2
Alexis J. Villegas <14046668+ajvillegas@users.noreply.github.com>
2024-03-18 15:50:21 -0400

    Add CSP allowed iframe origin rules for embedded media

--- client-mu-plugins/diff-security-plugin-integration.php
+++ client-mu-plugins/diff-security-plugin-integration.php
@@ -31,3 +31,21 @@ function maybe_add_local_media_proxy_origins( array $allowed_origins, string $po
 	return $allowed_origins;
 }
 add_filter( 'wmf/security/csp/allowed_origins', __NAMESPACE__ . '\\maybe_add_local_media_proxy_origins', 10, 2 );
+
+/**
+ * Add CSP allowed iframe origins to permit proxying embedded media requests.
+ *
+ * @param string[]  $allowed_origins List of origins to allow in this CSP.
+ * @param string    $policy_type     CSP type.
+ *
+ * @return string[] Filtered list of permitted origins.
+ */
+function permit_custom_iframe_providers( array $allowed_origins, string $policy_type ) : array {
+	if ( in_array( $policy_type, [ 'frame-src' ], true ) ) {
+		$allowed_origins[] = 'https://*.wikimedia.org';
+		$allowed_origins[] = 'https://datawrapper.dwcdn.net/';
+	}
+
+	return $allowed_origins;
+}
+add_filter( 'wmf/security/csp/allowed_origins', __NAMESPACE__ . '\\permit_custom_iframe_providers', 10, 2 );
